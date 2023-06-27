651a96a045ddbf6b74fd5cdec88de53b73e6672d
qwx <qwx@sciops.net>
2023-06-27 10:23:15 +0000

    dynar: add preallocation and clearing functions
    prealloc allows populating the array directly with pointers instead of push

--- util/dynar.h
+++ util/dynar.h
@@ -6,16 +6,21 @@ struct Dyhdr{
 	usize sz;
 };
 
+/* private, do not use directly */
 #define dyhdr(a)	((Dyhdr*)(a) - 1)
-#define dyfree(a)	(free(dyhdr(a)))
-#define dylen(a)	((a) == nil ? 0 : dyhdr(a)->len)
 #define dysetsz(a,h,n)	((a) = (void*)((h)+1), h->sz = (n), (a))
-#define dynew(a,h)	((h) = emalloc(4 * sizeof(*(a)) + sizeof(Dyhdr)), dysetsz((a), (h), 4))
+#define dynew(a,h,n)	((h) = emalloc((n) * sizeof(*(a)) + sizeof(Dyhdr)), dysetsz((a), (h), (n)))
 #define dyextend(a,h,n)	((h) = erealloc((h), \
 	2*n + sizeof(Dyhdr), n + sizeof(Dyhdr)), \
 	dysetsz((a), (h), n / sizeof(*(a)) * 2))
-#define dychecksz(a,h)	((a) == nil ? dynew((a),(h)) : \
+#define dychecksz(a,h)	((a) == nil ? dynew((a),(h),4) : \
 	(h)->sz == (h)->len ? dyextend((a), (h), sizeof(*(a)) * h->sz) : \
 	(a))
-#define dypush(a,v)	{Dyhdr*__h = dyhdr(a); (a) = dychecksz(a,__h); (a)[dyhdr(a)->len++] = v;}
+
+/* public */
+#define dyfree(a)	(free(dyhdr(a)))
+#define dylen(a)	((a) == nil ? 0 : dyhdr(a)->len)
+#define dypush(a,v)	do{Dyhdr*__h = dyhdr(a); (a) = dychecksz((a),__h); (a)[dyhdr(a)->len++] = v;}while(0)
 #define dypop(a)	(assert((a) != nil && dyhdr(a)->len > 0), (a)[--dyhdr(a)->len])
+#define dyclear(a)	do{if(a != nil){ memset((a), 0, dyhdr(a)->len * sizeof(*(a))); dyhdr(a)->len = 0;}while(0)
+#define dyprealloc(a,n)	do{Dyhdr*__h; dynew((a),__h,(n)); dyhdr(a)->len = (n); }while(0)
