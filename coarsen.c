21e2ebd6a9608603207e5d132e8b91196ef9686b
qwx <qwx@sciops.net>
2023-09-28 00:29:55 +0000

    coarsen: simpler, leaner, faster implementation and index format
    hinging on new em api

--- coarsen.c
+++ coarsen.c
@@ -4,267 +4,237 @@
 
 enum{
 	Minedges = 0,
-	IOunit = 64*1024,
+	Lrecsz = 4 * sizeof(u64int),
+	Nrecsz = 3 * sizeof(u64int),
+	Erecsz = 2 * sizeof(u64int),
 };
-static u64int N, M, S, L_M, NL, TN, TM;
-static File *noutf, *eoutf, *loutf;
-static uchar iobuf[IOunit];
-static EM *fedgeuv, *fedge, *fnode, *fweight, *flast, *flastm;
-
-static char *
-tmpname(usize level, int part)
-{
-	static char *pref, s[128];
-
-	if(pref == nil)
-		pref = sysmktmp();
-	snprint(s, sizeof s, "%s_%08zx_%d", pref, level, part);
-	return s;
-}
+typedef struct Lbuf Lbuf;
+struct Lbuf{
+	int nnodes;
+	int nedges;
+	EM *nodes;
+	EM *edges;
+};
 
 static int
-reverse(void)
+reverse(Graph *g, Lbuf *lvl)
 {
-	int i, x;
-	vlong noff, eoff;
-	u64int n, m;
-	File *f, *fi;
+	ssize noff, eoff;
+	Lbuf *lp, *le;
+	EM *em;
 
-	f = emalloc(sizeof *f);	// FIXME: unfuck the fucking api
-	if(fdopenfs(f, 1, OWRITE) < 0)
-		sysfatal("fdopenfs: %r");
-	fi = emalloc(sizeof *f);
-	put64(f, TN);
-	put64(f, TM);
-	put64(f, NL);
-	noff = (3 + 4*NL) * sizeof(u64int);
-	eoff = noff + 4*TN * sizeof(u64int);
-	// FIXME: dict could probably stay in memory, it's just 2*8 bytes per level
-	for(i=NL-1; i>=0; i--){
-		if(openfs(fi, tmpname(i, 0), OREAD) < 0)
-			sysfatal("openfs %r");
-		n = get64(fi);
-		m = get64(fi);
-		closefs(fi);	/* FIXME: fix this api */
-		sysremove(fi->path);
-		free(fi->path);
-		fi->path = nil;
-		put64(f, noff);
-		put64(f, eoff);
-		put64(f, n);
-		put64(f, m);
-		noff += n * 4 * sizeof(u64int);
-		eoff += m * 1 * sizeof(u64int);
+	/* FIXME: for a user, failing at this point is... disappointing. */
+	if((em = emfdopen(1)) == nil)
+		sysfatal("emfdopen: %s\n", error());
+	emput64(em, g->nnodes);
+	emput64(em, g->nedges);
+	emput64(em, dylen(lvl));
+	noff = em->off;
+	noff += dylen(lvl) * Lrecsz;
+	eoff = noff + Nrecsz;
+	for(le=lvl+dylen(lvl)-1, lp=le; lp>=lvl; lp--){
+		emput64(em, lp->nnodes);
+		emput64(em, lp->nedges);
+		emput64(em, noff);
+		emput64(em, eoff);
+		noff += lp->nnodes * Nrecsz;
+		eoff += lp->nedges * Erecsz;
 	}
-	for(x=1; x<3; x++)
-		for(i=NL-1; i>=0; i--){
-			if(openfs(fi, tmpname(i, x), OREAD) < 0)
-				sysfatal("openfs %r");
-			while((n = readfs(fi, iobuf, sizeof iobuf)) > 0)
-				writefs(f, iobuf, n);
-			closefs(fi);
-			sysremove(fi->path);
-			free(fi->path);
-			fi->path = nil;
-		}
-	freefs(f);
+	for(le=lvl+dylen(lvl)-1, lp=le; lp>=lvl; lp--){
+		emappend(em, lp->nodes);
+		emclose(lp->nodes);
+	}
+	for(le=lvl+dylen(lvl)-1, lp=le; lp>=lvl; lp--){
+		emappend(em, lp->edges);
+		emclose(lp->edges);
+	}
+	emflush(em);
+	emclose(em);
 	return 0;
 }
 
 static void
-printtab(void)
+printtab(EM *em, ssize ne)
 {
 	int i;
-	u64int u, v, e;
+	ssize u, v;
 
-	emseek(fedge, 0, 0);
-	for(i=0; i<M; i++){
-		e = emget64(fedge);
-		u = empget64(fedgeuv, e*8*2+2*8);
-		v = emget64(fedgeuv);
-		dprint(Debugcoarse, "E[%d] %llx → %llx,%llx\n", i, e, u, v);
+	if((debug & Debugcoarse) == 0)
+		return;
+	emseek(em, 0, 0);
+	for(i=0; i<ne; i++){
+		u = emget64(em);
+		v = emget64(em);
+		dprint(Debugcoarse, "E[%d] %llx,%llx\n", i, u, v);
 	}
 }
 
 static void
-endlevel(void)
+endlevel(Lbuf *lp, Graph *g)
 {
-	put64(loutf, N);
-	put64(loutf, L_M);
-	TN += N;
-	TM += L_M;
-	closefs(loutf);
-	free(loutf->path);
-	closefs(noutf);
-	free(noutf->path);
-	closefs(eoutf);
-	free(eoutf->path);
-	emnuke(flast);
-	emshrink(fedge, M*8);
-	eoutf->path = noutf->path = loutf->path = nil;
-}
-static void
-outputlevel(void)
-{
-	dprint(Debugcoarse, "ending level with N=%lld M=%lld\n", N, M);
-	endlevel();
+	g->nnodes += lp->nnodes;
+	g->nedges += lp->nedges;
 }
 
 static void
-outputedge(u64int e, u64int u, u64int v, u64int s, u64int t)
+outputedge(Lbuf *lp, ssize u, ssize v)
 {
-	put64(eoutf, e);
-	dprint(Debugcoarse, "discard edge i=%llx mapping to %llx,%llx (u,v %llx,%llx)\n", e, s, t, u, v);
-	L_M++;
+	EM *em;
+
+	dprint(Debugcoarse, "drop edge %llx,%llx\n", u, v);
+	em = lp->edges;
+	emput64(em, u);
+	emput64(em, v);
+	lp->nedges++;
 }
 
 static void
-outputnode(u64int u, u64int s, u64int news, u64int w)
+outputnode(Lbuf *lp, ssize old, ssize new, ssize weight)
 {
-	put64(noutf, u);
-	put64(noutf, s);
-	put64(noutf, news);
-	put64(noutf, w);
-	dprint(Debugcoarse, "merge node %llx (%llx) → %llx, weight %lld\n", u, s, news, w);
-	N++;
-}
-static void
-outputsuper(u64int u, u64int s, u64int news, u64int w)
-{
-	dprint(Debugcoarse, "new supernode %llx: ", s);
-	outputnode(u, s, news, w);
+	EM *em;
+
+	dprint(Debugcoarse, "merge %llx → %llx, weight %lld\n", old, new, weight);
+	em = lp->nodes;
+	emput64(em, old);
+	emput64(em, new);
+	emput64(em, weight);
+	lp->nnodes++;
 }
 
-static void
-newlevel(void)
+static Lbuf *
+newlevel(Lbuf *lvl)
 {
-	printtab();
-	N = M = L_M = 0;
-	dprint(Debugcoarse, "\t>> NEW LEVEL %lld\n", NL);
-	if(openfs(loutf, tmpname(NL, 0), OWRITE) < 0
-	|| openfs(noutf, tmpname(NL, 1), OWRITE) < 0
-	|| openfs(eoutf, tmpname(NL, 2), OWRITE) < 0)
-		sysfatal("newlevel %r");
-	NL++;
+	Lbuf l = {0};
+
+	l.nodes = emnew(0);
+	l.edges = emnew(0);
+	dypush(lvl, l);
+	dprint(Debugcoarse, "-- newlevel %lld\n", dylen(lvl));
+	return lvl;
 }
 
-static void
-outputtop(u64int u, u64int s, u64int news, u64int w)
-{
-	newlevel();
-	outputsuper(u, s, news, w);
-	outputlevel();
-}
-
-int
+static Lbuf *
 coarsen(Graph *g, char *index)
 {
-	int i, wu, wv;
-	u64int w, u, v, s, t, m, e, a, b;
-	ssize top;
+	ssize ls, lt, i, j, o, w, u, v, s, t, m, e, uw, vw, M, S;
+	EM *fedge, *fnode, *fweight, *fweight2, *flastpoke;
+	Lbuf *lvl, *lp;
 
-	if((fedgeuv = emopen(index)) == nil)
-		sysfatal("coarsen: %r");
-	fweight = emcreate(g->nnodes * sizeof(u64int));
-	fnode = emcreate(g->nnodes * sizeof(u64int));
-	fedge = emcreate(g->nedges * sizeof(u64int));
-	/* FIXME: these might never get big enough to need to be in external memory */
-	flastm = emcreate(g->nnodes * sizeof(u64int));
-	flast = emcreate(g->nnodes * sizeof(u64int));
-	dprint(Debugcoarse, "N %lld M %lld\n", N, M);
-	emseek(fweight, 0, 0);
-	emseek(fnode, 0, 0);
-	emseek(fedge, 0, 0);
-	for(i=0; i<g->nnodes; i++){
-		emput64(fweight, 1);
-		emput64(fnode, i);
-	}
-	for(i=0; i<g->nedges; i++)
-		emput64(fedge, i);
-	dprint(Debugcoarse, "N %lld M %lld\n", g->nnodes, g->nedges);
-	N = g->nnodes;
+	if((fedge = emopen(index)) == nil)
+		sysfatal("emclone: %s", error());
+	fweight = emnew(0);
+	fweight2 = emnew(0);
+	fnode = emnew(0);
+	flastpoke = emnew(0);
 	M = g->nedges;
-	S = N - 1;
+	S = g->nnodes - 1;
+	g->nnodes = 0;	/* vandalize it, don't care */
+	g->nedges = 0;
+	lvl = nil;
 	w = S;
-	top = -1;	/* cannot happen */
-	s = wu = 0;	/* cannot happen */
-	while(M > 0){
+	i = 0;
+	uw = -1;	/* cannot happen */
+	s = -1;	/* cannot happen */
+	while(M > Minedges){
+		printtab(fedge, M);
+		lvl = newlevel(lvl);
+		lp = lvl + dylen(lvl) - 1;
+		u = -1;
 		m = M;
-		newlevel();
-		/* FIXME: optimize (later) for less seeking/jumping around */
-		emseek(fedge, 0, 0);
-		emseek(fedgeuv, 2*8, 0);
-		for(i=0; i<m; i++){
-			e = empget64(fedge, i*8);
-			u = empget64(fedgeuv, e*8*2+2*8);
-			v = emget64(fedgeuv);
-			s = empget64(fnode, u*8);
-			dprint(Debugcoarse, "getnode %d %zd %zd %zd s %zd\n", i, e, u, v, s);
-			/* unvisited node: make new supernode */
-			if(s <= w){
-				if(top >= 0)
-					empput64(fweight, top*8, wu);
-				top = u;
-				S++;
-				wu = empget64(fweight, u*8);
-				outputsuper(u, s, S, wu);
-				empput64(flastm, (s-(s>=g->nnodes?g->nnodes:0))*8, S);
-				s = S;
-				empput64(flast, (s-w)*8, i);
-				empput64(fnode, u*8, s);
-			}
-			t = empget64(fnode, v*8);
-			a = t >= g->nnodes ? empget64(flastm, (t-g->nnodes)*8) : 0; 
-			/* unvisited adjacency or self: merge internal edges */
-			dprint(Debugcoarse, "check for redundancy: top %llx u %llx v %llx s %llx t %llx w %lld lastm[t-N] %llx\n", top, u, v, s, t, w, a);
-			if(t <= w && a <= w || t == s && u != v){
-				/* edges not starting from the top node are mirrors, skip them */
-				if(u == top){
-					wv = empget64(fweight, v*8);
-					outputnode(v, t, s, wv);
-					wv += wu;
-					wu = wv;
-					empput64(fweight, v*8, wv);
-					empput64(fnode, v*8, s);
-					outputedge(e, u, v, s, t);
+		M = 0;
+		/* FIXME: could directly skip the second half of the list in the first
+		 * iteration? that'd be a big speed up */
+		for(e=0; e<m; e++){
+			/* new left node */
+			if((o = emget64(fedge)) != u){
+				u = o;
+				s = empget64(fnode, u);
+				dprint(Debugcoarse, "[%04llx] new left node %llx → %llx\n", e, u, s);
+				i = s - 1 - w;
+				/* buffer contents are > w or undefined */
+				if(s <= w){
+					dprint(Debugcoarse, "[%04llx] unvisited left node: %llx ← %llx\n",
+						e, u, S+1);
+					s = ++S;
+					i = s - 1 - w;
+					uw = empget64(fweight, u);
+					outputnode(lp, u, s, uw);
+					empput64(fnode, u, s);
+					empput64(fweight2, i, uw);
+					empput64(flastpoke, i, i);
 				}
-			/* adjacency previously merged elsewhere: fold external edges */
-			}else{
-				a = t >= w ? empget64(flast, (t-w)*8) : 0;
-				b = s >= w ? empget64(flast, (s-w)*8) : 0;
-				dprint(Debugcoarse, "check for redundancy 2: a %llx b %llx NL %lld\n",
-					a, b, NL);
-				if(a >= b && (u != v || NL > 1)){
-					/* already retained one edge, discard following redundant ones */
-					dprint(Debugcoarse, "discarding redundant edge: t:%llx >= s:%llx\n", a, b);
-					outputedge(e, u, v, s, t);
+			}
+			v = emget64(fedge);
+			t = empget64(fnode, v);
+			dprint(Debugcoarse, "[%04llx] edge %llx→%llx,%llx→%llx\n", e, u, s, v, t);
+			if(t <= w){
+				dprint(Debugcoarse, "[%04llx] unvisited right node: %llx ← %llx\n",
+					e, v, s);
+				vw = empget64(fweight, v);
+				outputnode(lp, v, u, vw);
+				uw += vw;
+				empput64(fweight2, i, uw);
+				outputedge(lp, u, v);
+			}else if(v == u){
+				dprint(Debugcoarse, "[%04llx] self edge: %llx,%llx\n", e, u, s);
+				outputedge(lp, u, u);
+			}else if(t == s)
+				dprint(Debugcoarse, "[%04llx] mirror of previous edge, ignored\n", e);
+			else{
+				j = t - 1 - w;
+				lt = empget64(flastpoke, j);
+				ls = empget64(flastpoke, i);
+				assert(lt >= 0 && ls >= 0);
+				/* to avoid multiple edges between the same two supernodes, only
+				 * admit one iff super-u was marked mapped before super-v;
+				 * multiple u,super-v edges and super-v,super-u are prohibited */
+				if(ls < lt){
+					dprint(Debugcoarse, "[%04llx] new external edge (%lld < %lld)\n",
+						e, ls, lt);
+					empput64(flastpoke, j, i);
+					empput64(fedge, 2*M++, s);
+					emput64(fedge, t);
 				}else{
-					/* retain edge for next round */
-					empput64(fedge, M*8, e);
-					M++;
-					dprint(Debugcoarse, "retain edge[%x] %llx,%llx at %llx slot %lld\n", i, s, t, a, M);
-					empput64(flast, (t-(t>=w?w:g->nnodes))*8, i);
+					dprint(Debugcoarse, "[%04llx] redundant external edge\n", e);
+					outputedge(lp, u, v);
 				}
 			}
 		}
-		outputlevel();
+		emshrink(fnode, 8*(S-w));
+		emshrink(fedge, 8*2*M);
+		emshrink(flastpoke, 0);
+		emflip(fweight, fweight2);
+		endlevel(lp, g);
 		w = S;
 	}
-	/* add one node to rule them all if none remain */
-	if(M > 0)
-		dprint(Debugcoarse, "stopping at %lld remaining edges\n", M);
-	printtab();
-	if(N > 0){
-		dprint(Debugcoarse, "push artificial node to rule all remaining");
-		outputtop(top, s, ++S, wu);
+	if(M > 0){
+		dprint(Debugcoarse, "stopping with %lld remaining edges\n", M);
+		printtab(fedge, M);
 	}
-	emclose(fedgeuv);
+	/* add articial root node for all remaining */
+	if(S > w + 1){
+		s = ++S;
+		dprint(Debugcoarse, "push artificial root node %llx\n", s);
+		lvl = newlevel(lvl);
+		lp = lvl + dylen(lvl) - 1;
+		emseek(fedge, 0, 0);
+		/* every node is an adjacency, every edge is internal */
+		for(e=0, u=-1; e<M; e++){
+			if((o = emget64(fedge)) != u){
+				u = o;
+				w = empget64(fweight, u);
+				outputnode(lp, u, s, w);
+			}
+			v = emget64(fedge);
+			outputedge(lp, u, v);
+		}
+	}
+	emclose(fedge);
 	emclose(fnode);
 	emclose(fweight);
-	emclose(fedge);
-	emclose(flast);
-	emclose(flastm);
-	return 0;
+	emclose(fweight2);
+	emclose(flastpoke);
+	return lvl;
 }
 
 static int
@@ -274,10 +244,11 @@ readindex(Graph *g, char *path)
 
 	f = emalloc(sizeof *f);
 	if(openfs(f, path, OREAD) < 0)
-		sysfatal("readindex: %r");
+		sysfatal("readindex: %s", error());
 	g->nnodes = get64(f);
 	g->nedges = get64(f);
-	dprint(Debugcoarse, "N %lld M %lld\n", g->nnodes, g->nedges);
+	dprint(Debugcoarse, "graph %s nnodes %lld nedges %lld\n",
+		f->path, g->nnodes, g->nedges);
 	freefs(f);
 	return 0;
 }
@@ -285,7 +256,7 @@ readindex(Graph *g, char *path)
 static void
 usage(void)
 {
-	dprint(Debugcoarse, "usage: %s SORTED_EDGES\n", argv0);
+	dprint(Debugcoarse, "usage: %s [-D which] SORTED_EDGES\n", argv0);
 	sysfatal("usage");
 }
 
@@ -294,6 +265,7 @@ main(int argc, char **argv)
 {
 	char *s;
 	Graph g;
+	Lbuf *lvl;
 
 	ARGBEGIN{
 	case 'D':
@@ -321,13 +293,10 @@ main(int argc, char **argv)
 	if(argc < 1)
 		usage();
 	if(readindex(&g, argv[0]) < 0)
-		sysfatal("readindex: %r");
-	loutf = emalloc(sizeof *loutf);
-	noutf = emalloc(sizeof *noutf);
-	eoutf = emalloc(sizeof *eoutf);
-	if(coarsen(&g, argv[0]) < 0)
-		sysfatal("coarsen: %r");
-	if(reverse() < 0)
-		sysfatal("reverse: %r");
+		sysfatal("readindex: %s", error());
+	if((lvl = coarsen(&g, argv[0])) == nil)
+		sysfatal("coarsen: %s", error());
+	if(reverse(&g, lvl) < 0)
+		sysfatal("reverse: %s", error());
 	return 0;
 }
