a6f116c83396084c11e44ccc196b067fffcc6a2c
qwx <qwx@sciops.net>
2023-10-14 07:03:49 +0000

    dynar: fix typo in dyclear, grow buffer correctly in dyinsert

--- util/dynar.h
+++ util/dynar.h
@@ -13,23 +13,23 @@ struct Dyhdr{
 #define dyextend(a,h,n)	((h) = erealloc((h), \
 	2*(n) + sizeof(Dyhdr), (n) + sizeof(Dyhdr)), \
 	dysetsz((a), (h), (n) / sizeof(*(a)) * 2))
-#define dychecksz(a,h)	((a) == nil ? dynew((a),(h),4) : \
-	(h)->sz == (h)->len ? dyextend((a), (h), sizeof(*(a)) * (h)->sz) : \
+#define dychecksz(a,h,i)	((a) == nil ? dynew((a),(h),4) : \
+	(h)->sz <= (i) ? dyextend((a), (h), sizeof(*(a)) * (h)->sz) : \
 	(a))
 
 /* public */
 #define dyfree(a)	(free(dyhdr(a)))
 #define dylen(a)	((a) == nil ? 0 : dyhdr(a)->len)
 #define dyavail(a)	((a) == nil ? 0 : dyhdr(a)->sz - dyhdr(a)->len)
-#define dypush(a,v)	do{Dyhdr*__h = dyhdr(a); (a) = dychecksz((a),__h); (a)[dyhdr(a)->len++] = v;}while(0)
+#define dypush(a,v)	do{Dyhdr*__h = dyhdr(a); (a) = dychecksz((a),__h,__h->len); (a)[dyhdr(a)->len++] = v;}while(0)
 #define dypop(a)	do{assert((a) != nil && dyhdr(a)->len > 0); --dyhdr(a)->len;}while(0)
-#define dyclear(a)	do{if((a) != nil){ memset((a), 0, dyhdr(a)->len * sizeof(*(a))); dyhdr(a)->len = 0;}while(0)
+#define dyclear(a)	do{if((a) != nil){ memset((a), 0, dyhdr(a)->len * sizeof(*(a))); dyhdr(a)->len = 0;}}while(0)
 #define dyprealloc(a,n)	do{Dyhdr*__h; (a) = dynew((a),__h,(n)); dyhdr(a)->len = (n); }while(0)
 
 /* FIXME: starting to suck big time */
 #define dygrow(a,i)	do{ \
 	Dyhdr*__h = dyhdr(a); \
-	(a) = dychecksz((a),__h); \
+	(a) = dychecksz((a),__h,__h->len); \
 	while(__h->sz < (i))	\
 		(a) = dyextend((a), __h, sizeof(*(a)) * __h->sz);	\
 	}while(0)
@@ -46,11 +46,13 @@ struct Dyhdr{
 /* one may not simply use dygrow here with the do..while */
 #define dyinsert(a,i,v)	do{ \
 	Dyhdr*__h = dyhdr(a); \
-	(a) = dychecksz((a),__h); \
+	(a) = dychecksz((a),__h,(i)); \
 	while(__h->sz < (i))	\
 		(a) = dyextend((a), __h, sizeof(*(a)) * __h->sz);	\
 	if((i) == dyhdr(a)->len) \
 		dypush((a),(v)); \
 	else \
 		(a)[(i)] = (v); \
+	if((i) > __h->len) \
+		__h->len = (i); \
 	}while(0)
