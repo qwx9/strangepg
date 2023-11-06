d2c9009d396e7c2bb9e96a9a896419b79de48447
qwx <qwx@sciops.net>
2023-11-06 14:48:37 +0000

    test: add more ct tests: stress testing page alloc and flushing

---
+++ test/testct07.c
@@ -0,0 +1,20 @@
+#include "strpg.h"
+#include "em.h"
+
+int
+main(int argc, char **argv)
+{
+	ssize r, i, v, w;
+	EM *em;
+
+	em = emopen(nil, 0);
+	for(i=0; i<10000000; i++){
+		r = lrand() % ((1<<16)-1);
+		v = r | r<<16 | r<<32ULL | r<<48ULL;
+		emw64(em, r, v);
+		if((w = emr64(em, r)) != v)
+			sysfatal("emr64: %zx not %zx %s", v, w, error());
+	}
+	emclose(em);
+	sysquit();
+}
---
+++ test/testct09.c
@@ -0,0 +1,21 @@
+#include "strpg.h"
+#include "em.h"
+
+int
+main(int argc, char **argv)
+{
+	ssize r, i, v, w;
+	EM *em;
+
+	if((em = emopen(nil, 0)) == nil)
+		sysfatal("emopen: %s", error());
+	for(i=0; i<10000000; i++){
+		r = lrand() % (((1ULL<<32)-1)/4);
+		v = r | r<<32ULL;
+		emw64(em, r, v);
+		if((w = emr64(em, r)) != v)
+			sysfatal("emr64: %zx not %zx %s", v, w, error());
+	}
+	emclose(em);
+	sysquit();
+}
