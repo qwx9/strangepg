1403e284e24bd874ae8a10966b12c39c944f0a9f
qwx <qwx@sciops.net>
2023-11-13 05:41:39 +0000

    index.sh: some configurability incl. reading from stdin, and cleanup

--- index.sh
+++ index.sh
@@ -1,37 +1,34 @@
-#!/bin/sh -e
+#!/bin/bash -e
 # as explicit as possible, can certainly be shortened;
 # adding mirrors makes sure that all edges u,v can be processed like this,
 # labeling every node, and making sure indices are consistent between u and v
-# final format:
-#	index: hdr[] elist[]
-#	hdr: [nn][ne]
-# 	elist: {[u][v]}[ne]
-# umap is literally a map u→s; ordering is set in the ct, here it's useless
-# elist is sorted by degree(u), then u, then v
 
-# FIXME: general usage
-#tmpdir=/tmp
-tmpdir=$HOME/tmp
-tmp="$tmpdir/`uuidgen`"
+# FIXME: general usage; document
+uuid=`uuidgen`
+tmpdir=$TMPDIR
+out=$1
+if [[ "x$tmpdir" == x ]]; then tmpdir=$HOME/tmp; fi
+if [[ "x$out" == "x-" ]]; then out="stdin.$uuid"; fi
+mkdir -p $tmpdir
+tmp="$tmpdir/$uuid"
 
 awk 'BEGIN{OFS="\t"}$1=="L"{print $2, $4 "\n" $4, $2}' "$1" \
-	| sort -k1,2V -u -T $tmpdir -S 11G \
+	| sort -k1,2V -u -T $tmpdir -S 10G \
 	| awk 'BEGIN{OFS="\t"}{if(u!=$1) n++; u=$1; print n, $1, $2}' \
 	> $tmp.ulist
 
 # FIXME: is this strictly necessary?? check for duplicate work with named
 # non sequential links/nodes
-sort -k3V -k2V $tmp.ulist -T $tmpdir -S 11G \
+sort -k3V -k2V $tmp.ulist -T $tmpdir -S 10G \
 	| paste $tmp.ulist - \
 	| awk '{if(u!=$1){u=$1; printf "%s%s\t", (NR==1?"":"\n"), u}; printf "%s\t", $4}END{printf "\n"}' \
 	| awk 'BEGIN{OFS="\t"}{print NF-1, $0}' \
-	| sort -k1V -k2V -k3V -T $tmpdir -S 11G \
+	| sort -k1V -k2V -k3V -T $tmpdir -S 10G \
 	> $tmp.elist
 
 rm -f $tmp.ulist
 
-/bin/time -f "py: %E usr %U sys %S max %Mk avg %Kk fs %O" \
-	<$tmp.elist /usr/bin/env python3 -c '
+<$tmp.elist /usr/bin/env python3 -c '
 import fileinput
 import sys
 from struct import pack
@@ -51,6 +48,6 @@ f = sys.stdout.buffer
 f.write(pack("Q", nn))
 f.write(pack("Q", ne))
 edges.close()
-' > "$1.idx"
-cat $tmp >> "$1.idx" && rm $tmp
-mv $tmp.elist "$1.idxe"
+' > "$out.idx"
+cat $tmp >> "$out.idx" && rm $tmp
+mv $tmp.elist "$out.idxe"
