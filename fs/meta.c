62bdcc5211b9a5b35b813b3abd32f840b89fd438
qwx <qwx@sciops.net>
2024-03-01 12:01:38 +0000

    fs/meta: parse edges, fix wrong var in pushcmd format
    duplicated code.

--- fs/meta.c
+++ fs/meta.c
@@ -2,8 +2,6 @@
 #include "fs.h"
 #include "cmd.h"
 
-// FIXME: don't clearmeta(g) in loadgfa
-
 // FIXME: this symbolically should really be just a bunch of hash tables
 //	key == variable + index -> var:i --h--> NA, cached, or loadreq
 //	functions the exact same way as EM in a way...
@@ -11,19 +9,24 @@
 //	lighter alternative for now: pass it to awk and make it parse and store
 //	all metadata
 
-// so design:
+// so design (fawk? 0wk?):
 // hashmap of keys maps to index in registry, linking corresponding table
 //	either flat full EM-based table, or hash table, doesn't matter which
 //	(make queries transparent)
 
-// but, in practice, until we get our new not-fawk:
-// parse meta in separate worker and feed to awk
-
-// awk side: execute request and show result
-
 // add status messages => first cimgui stuff
 
 // toggle node/edge labels on/off
+
+// compile new script with existing variables, or just hardcode known ones
+// alt: reading the script is fast, reading the data is slow -> just rerun?
+//	plays right into fawk's hands
+
+// we disallow setting/creating new variables
+//	blue would be a variable so we don't have to type "blue"
+// goal: color[NC == 3] = blue
+//	-> ?u
+// also: load from csv/...
 
 
 // FIXME: more abstractions, fp -> gfa.c
@@ -34,14 +37,12 @@ collectgfanodes(Graph *g, File *f)
 	int l, nerr;
 	Node *n;
 
-	// FIXME: wrong id??
 	for(n=g->nodes, nerr=0; n<g->nodes+dylen(g->nodes); n++){
 		seekfs(f, n->metaoff);
-		warn("node %zx off %lld\n", n->id, n->metaoff);
 		if((s = readline(f, &l)) == nil){
 			warn("collectmeta: %s\n", error());
 			if(++nerr > 10){
-				werrstr("collectmeta: too many errors");
+				werrstr("too many errors");
 				return -1;
 			}
 			continue;
@@ -53,7 +54,8 @@ collectgfanodes(Graph *g, File *f)
 		// FIXME: truncated lines
 		l = t != nil ? t - s : strlen(s);
 		if(l > 1 || l == 1 && s[0] != '*')
-			pushcmd("s NLN %d %d", n->id, n);
+			pushcmd("s NLN %d %d", n->id, l);
+		// FIXME: don't overwrite LN from here
 		for(s=t; s!=nil; s=t){
 			t = getfield(s);
 			l = t != nil ? t - s : strlen(s);
@@ -67,25 +69,61 @@ collectgfanodes(Graph *g, File *f)
 	return 0;
 }
 
-// FIXME: more abstractions, fp -> gfa.c
+static int
+collectgfaedges(Graph *g, File *f)
+{
+	char *s, *t;
+	int l, nerr;
+	Edge *e;
+
+	for(e=g->edges, nerr=0; e<g->edges+dylen(g->edges); e++){
+		seekfs(f, e->metaoff);
+		if((s = readline(f, &l)) == nil){
+			warn("collectmeta: %s\n", error());
+			if(++nerr > 10){
+				werrstr("too many errors");
+				return -1;
+			}
+			continue;
+		}
+		if((s = getfield(s)) == nil		/* L */
+		|| (s = getfield(s)) == nil		/* u */
+		|| (s = getfield(s)) == nil		/* du */
+		|| (s = getfield(s)) == nil		/* v */
+		|| (s = getfield(s)) == nil)	/* dv */
+			continue;
+		t = getfield(s);
+		// FIXME: truncated lines
+		l = t != nil ? t - s : strlen(s);
+		if(l > 1 || l == 1 && s[0] != '*')
+			pushcmd("s cig %d %s", e->id, s);
+		for(s=t; s!=nil; s=t){
+			t = getfield(s);
+			l = t != nil ? t - s : strlen(s);
+			if(l < 6 || s[2] != ':' || s[4] != ':')
+				continue;
+			s[2] = 0;
+			pushcmd("s L%s %d %s", s, e->id, s+5);
+		}
+		nerr = 0;
+	}
+	return 0;
+}
+
+// FIXME: more abstractions, fp -> gfa.c (fs.c load structs)
 void
 collectgfameta(Graph *g)
 {
 	File *f;
 
+	clearmeta(g);	/* delegated to awk */
 	g->loading = 1;
 	f = g->f;
-	//clearmeta(g);	/* delegated to awk */
+	// FIXME: wait until meta is loaded to launch meta commands
 	if(collectgfanodes(g, f) < 0)
 		warn("collectmeta: %s\n", error());
-
-
-// FIXME: compile awk script with existing variables
-// do simple trial
-// goal: color nodes
-// alt: reading the script is fast, reading the data is slow -> just rerun?
-//	plays right into fawk's hands
-
+	if(collectgfaedges(g, f) < 0)
+		warn("collectmeta: %s\n", error());
 	g->loading = 0;
 }
 
