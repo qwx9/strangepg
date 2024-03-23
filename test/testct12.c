89c110ceba0a98d8c4fd49d781e0aa71198ece98
qwx <qwx@sciops.net>
2024-03-23 11:03:09 +0000

    add testct12: random read-write test

---
+++ test/testct12.c
@@ -0,0 +1,45 @@
+#include "strpg.h"
+#include "em.h"
+
+enum{
+	Niter = 10000,
+	M = 29,
+	R = 30,
+};
+
+int
+main(int, char **)
+{
+	ssize r, i, v, w;
+	EM *em;
+
+	//debug |= Debugextmem;
+	multiplier = M;
+	initem();
+	if((em = emopen(nil, 0)) == nil)
+		sysfatal("emopen: %s", error());
+	srand(42);
+		vlong t = nsec();
+	for(i=0; i<Niter; i++){
+		if((i+1) % (1<<8) == 0)
+			warn("w64[%zd]\n", i);
+		r = lrand() % (((1ULL<<R)-1));
+		v = r | r<<32ULL;
+		emw64(em, r, v);
+	}
+	warn("writes: %lld msec\n", (nsec() - t) / 1000000);
+	srand(42);
+	t = nsec();
+	for(i=0; i<Niter; i++){
+		if((i+1) % (1<<8) == 0)
+			warn("r64[%zd]\n", i);
+		r = lrand() % (((1ULL<<R)-1));
+		v = r | r<<32ULL;
+		if((w = emr64(em, r)) != v)
+			sysfatal("emr64: [%zx] read %zx, expected %zx", i, w, v);
+	}
+	warn("reads: %lld msec\n", (nsec() - t) / 1000000);
+	emclose(em);
+	sysquit();
+	return 0;
+}
