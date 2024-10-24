0886426f4fa120f8fd8ec3635f381f4d9617beaa
qwx <qwx@sciops.net>
2024-10-24 00:03:25 +0000

    fs/layout: extend to 3 dimensions

--- fs/layout.c
+++ fs/layout.c
@@ -7,7 +7,7 @@
 #include "cmd.h"
 
 enum{
-	Recsz = 2 * sizeof(float),
+	Recsz = 3 * sizeof(float),
 };
 
 int
@@ -43,6 +43,9 @@ importlayout(Graph *g, char *path)
 		p += sizeof u.u;
 		u.u = GBIT32(p);
 		r->pos[1] = u.f;
+		p += sizeof u.u;
+		u.u = GBIT32(p);
+		r->pos[2] = u.f;
 	}
 end:
 	if(x > 0 && x < sizeof buf){
@@ -78,6 +81,9 @@ exportlayout(Graph *g, char *path)
 		PBIT32(p, u.u);
 		p += sizeof u.u;
 		u.f = r->pos[1];
+		PBIT32(p, u.u);
+		p += sizeof u.u;
+		u.f = r->pos[2];
 		PBIT32(p, u.u);
 		if(writefs(fs, buf, sizeof buf) < 0)
 			goto end;
