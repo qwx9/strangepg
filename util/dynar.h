09f6cf924f06fffd7eb6473ee9f90725160519e3
qwx <qwx@sciops.net>
2025-03-11 10:07:07 +0000

    dynar: fix some of the alignment issues, add dyshrink, minor fixes

--- util/dynar.h
+++ util/dynar.h
@@ -1,55 +1,101 @@
 /* ideas from klib and stb */
 typedef struct Dyhdr Dyhdr;
 struct Dyhdr{
-	usize sz;
-	usize len;
+	ssize sz;
+	ssize len;
+	char align[];
 };
 
+/* FIXME: most of this shit doesn't really need to be a macro at all,
+ * at least use inline */
+/* FIXME: could we do just ssize[2] instead of a struct? 
+ * so -1 would be len, -2 would be sz?
+ * or align to 8 or 16 bytes? + 7 & ~7 */
+
+/* FIXME: non portable */
+#define alignof(x)	offsetof(struct{ char __u; __typeof__(x) __v; }, __v)
+
 /* private, do not use directly */
-#define dyhdr(a)	((Dyhdr*)(a) - 1)
-#define dysetsz(a,h,n)	((a) = (void*)((h)+1), (h)->sz = (n), (a))
-#define dynew(a,h,n)	((h) = emalloc((n) * sizeof(*(a)) + sizeof(Dyhdr)), dysetsz((a), (h), (n)))
-#define dyextend(a,h,n)	((h) = erealloc((h), \
-	2*(n) + sizeof(Dyhdr), (n) + sizeof(Dyhdr)), \
-	dysetsz((a), (h), (n) / sizeof(*(a)) * 2))
-#define dychecksz(a,h,i)	((a) == nil ? dynew((a),(h),4) : \
-	(h)->sz <= (i) ? dyextend((a), (h), sizeof(*(a)) * (h)->sz) : \
-	(a))
+#define	dyalign(a)	((uchar*)(a) - alignof(a))
+#define	dyhsz(a)	(sizeof(Dyhdr) + alignof(a))
+
+#define dyhdr(a)	(((Dyhdr*)(dyalign(a))) - 1)
+#define dysz(a)	((a) == nil ? 0 : dyhdr(a)->sz)
+
+/* FIXME: UGH */
+#define dysetsz(a,h,n)	do{ \
+	(a) = (__typeof__(a))((uchar*)(h)+dyhsz(a)); \
+	(h)->sz = (n); \
+}while(0)
+#define dynew(a,h,n)	do{ \
+	(h) = emalloc( \
+		(n) * sizeof(*(a)) + sizeof(Dyhdr) + alignof(a)); \
+	dysetsz((a), (h), (n)); \
+}while(0)
+#define dyextend(a,h)	do{ \
+	(h) = erealloc((h), \
+		2 * (h)->sz * sizeof(*(a)) + dyhsz(a), \
+		(h)->sz * sizeof(*(a)) + dyhsz(a)); \
+	dysetsz((a), (h), 2*(h)->sz); \
+}while(0)
 
 /* public */
+#define dylen(a)	((a) == nil ? 0 : dyhdr(a)->len)
+#define dyavail(a)	((a) == nil ? 0 : dyhdr(a)->sz - dyhdr(a)->len)
+
 #define dyfree(a)	do{ \
 	if((a) != nil){ \
 		free(dyhdr(a)); \
 		(a) = nil; \
-	}}while(0)
-#define dylen(a)	((a) == nil ? 0 : dyhdr(a)->len)
-#define dyavail(a)	((a) == nil ? 0 : dyhdr(a)->sz - dyhdr(a)->len)
-#define dypush(a,v)	do{Dyhdr*__h = dyhdr(a); (a) = dychecksz((a),__h,__h->len); (a)[dyhdr(a)->len++] = (v);}while(0)
+	} \
+}while(0)
+#define dypush(a,v)	do{ \
+	Dyhdr *__h; \
+	if((a) == nil) \
+		dynew((a), __h, 4); \
+	else{ \
+		__h = dyhdr(a); \
+		if(__h->sz <= __h->len) \
+			dyextend((a), __h); \
+	} \
+	(a)[__h->len++] = (v); \
+}while(0)
 #define dypop(a)	do{assert((a) != nil && dyhdr(a)->len > 0); --dyhdr(a)->len;}while(0)
 #define dyclear(a)	do{if((a) != nil){ memset((a), 0, dyhdr(a)->len * sizeof(*(a))); dyhdr(a)->len = 0;}}while(0)
 #define dyreset(a)	do{if((a) != nil){ dyhdr(a)->len = 0;}}while(0)
-#define dyprealloc(a,n)	do{Dyhdr*__h; (a) = dynew((a),__h,(n)); dyhdr(a)->len = (n); }while(0)
+#define dyprealloc(a,n)	do{Dyhdr*__h; dynew((a),__h,(n)); dyhdr(a)->len = (n); }while(0)
 
-/* FIXME: starting to suck big time */
 #define dygrow(a,i)	do{ \
 	if((a) == nil){ \
 		dyprealloc(a, (i)); \
 		break; \
 	} \
 	Dyhdr*___h = dyhdr(a); \
-	if(___h->sz <= (i)){ \
-		(a) = dychecksz((a),___h,(i)); \
-		while(___h->sz <= (i))	\
-			(a) = dyextend((a), ___h, sizeof(*(a)) * ___h->sz);	\
-	}}while(0)
+	while(___h->sz <= (i)) \
+		dyextend((a), ___h);	\
+}while(0)
 #define dyresize(a,i) do{ \
-	Dyhdr*_h = dyhdr(a); \
-	if((a) == nil || _h->sz < (i)){ \
+	Dyhdr*_h; \
+	if((a) == nil || (_h = dyhdr(a))->sz < (i)){ \
 		dygrow(a, (i)); \
 		_h = dyhdr(a); \
 		_h->len = (i); \
 	}else if(_h->len < (i)) \
 		_h->len = (i); \
+	}while(0)
+#define dyshrink(a,n) do{ \
+	Dyhdr*_h; \
+	if((a) != nil){ \
+		_h = dyhdr(a); \
+		if((n) <= _h->sz / 2){ \
+			_h = erealloc(_h, \
+				(n) * sizeof(*(a)) + dyhsz(a), \
+				_h->sz * sizeof(*(a)) + dyhsz(a)); \
+			dysetsz(a,_h,n); \
+		} \
+		if(_h->len > (n)) \
+			_h->len = (n); \
+	} \
 	}while(0)
 #define dyinsert(a,i,v)	do{ \
 	dyresize(a, (i)+1); \
