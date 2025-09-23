6bec52327d80b9485f65ba86eac10d80d989c832
qwx <qwx@sciops.net>
2025-09-23 11:58:46 +0000

    sokol: move extra declarations to impl file
    another breakage in cygwin build

--- sokol/impl_sokol_gfx.c
+++ sokol/impl_sokol_gfx.c
@@ -3,6 +3,15 @@
 #include <signal.h>
 #define	SOKOL_IMPL
 #define	SOKOL_NO_ENTRY
+
+#define GL_VIEWPORT 0x0BA2
+#define GL_DEBUG_OUTPUT 0x92E0
+#define GL_DEBUG_TYPE_ERROR 0x824C
+#define SG_GL_FUNCS_EXT	\
+	_SG_XMACRO(glDebugMessageCallback,	void, (GLDEBUGPROC callback, const void * userParam)) \
+	_SG_XMACRO(glGetTexImage,	void, (GLenum target, GLint level, GLenum format, GLenum type, void * pixels)) \
+	_SG_XMACRO(glReadPixels,	void, (GLint x, GLint y, GLsizei width, GLsizei height, GLenum format, GLenum type, void * pixels)) \
+
 #include "lib/sokol_app.h"
 #include "lib/sokol_gfx.h"
 #include "lib/sokol_log.h"
