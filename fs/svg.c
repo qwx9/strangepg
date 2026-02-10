ad3ab109fd974f17f6e4d310228fa204c0ec75a3
qwx <qwx@sciops.net>
2026-02-10 21:01:28 +0000

    fs/svg: improve output somewhat
    viewbox still not great

--- fs/svg.c
+++ fs/svg.c
@@ -6,12 +6,11 @@
 #include "layout.h"
 #include "threads.h"
 
-/* FIXME: have to do a 2d projection on the screen or sth */
 int
 exportsvg(char *path)
 {
 	char buf[1024], tail[256], *p;
-	float c, s, w, θ, dim[4] = {9999.0f, 9999.0f, 0.0f, 0.0f};
+	float c, s, l, w, θ, dim[4] = {9999.0f, 9999.0f, 0.0f, 0.0f};
 	u32int col;
 	RNode *r, *re;
 	REdge *e, *ee;
@@ -25,9 +24,10 @@ exportsvg(char *path)
 		lsleep(1000);
 	if((fs = openfs(path, OWRITE)) == nil)
 		return -1;
-	/* FIXME: ideally done in draw or w/e */
+	/* FIXME: doesn't take into account transformations */
 	w = MAX(drawing.nodesz, drawing.fatness);
 	for(r=rnodes, re=r+dylen(r); r<re; r++){
+		//l = r->len + w;
 		if(r->pos[0] - w < dim[0])
 			dim[0] = r->pos[0] - w;
 		else if(r->pos[0] + w > dim[2])
@@ -57,7 +57,7 @@ exportsvg(char *path)
 		col |= ((int)(r->col[1] / r->col[3] * 255.f) & 0xff) << 8;
 		col |= ((int)(r->col[2] / r->col[3] * 255.f) & 0xff);
 		p = seprint(buf, buf+sizeof buf,
-			"<rect transform=\"translate(%f %f) rotate(%f) scale(%f 1)\" "
+			"<rect transform=\"scale(1 -1) translate(%f %f) rotate(%f) scale(%f 1)\" "
 			"fill=\"#%06x\" fill-opacity=\"%f\" ",
 			r->pos[0], r->pos[1], θ, r->len, col, r->col[3]);
 		p = strecpy(p, buf+sizeof buf, tail);
@@ -69,8 +69,8 @@ exportsvg(char *path)
 	for(e=redges, ee=e+dylen(e); e<ee; e++){
 		p = seprint(buf, buf+sizeof buf,
 			"<line x1=\"%f\" y1=\"%f\" x2=\"%f\" y2=\"%f\" "
-			"style=\"stroke:#cccccc;stroke-opacity:0.5;stroke-width:1\"/>\n",
-			e->pos1[0], e->pos1[1], e->pos2[0], e->pos2[1]);
+			"style=\"stroke:#cccccc;stroke-opacity:0.3;stroke-width:0.1\"/>\n",
+			e->pos1[0], -e->pos1[1], e->pos2[0], -e->pos2[1]);
 		if(writefs(fs, buf, p - buf) < 0){
 			logerr(va("exportsvg: %s\n", error()));
 			goto end;
